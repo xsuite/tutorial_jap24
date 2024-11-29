0d0b71a24dc908d58493177683452aaec98ef943
giadarol <giovanni.iadarola@cern.ch>
2024-11-29 16:23:26 +0100

    Not bad...

--- 003_fit_model.py
+++ 003_fit_model.py
@@ -33,22 +33,60 @@ line['px_inj'] = 0.
 line['delta_inj'] = 0.
 line['zeta_inj'] = 0.
 
-line.vars.update({
- 'x_inj': -0.0005635430558554506,
- 'px_inj': -9.739671862890309e-06,
- 'delta_inj': 0.0005548018233742175,
- 'zeta_inj': -0.34736514218425374})
+# line.vars.update({
+#  'x_inj': -0.0005635430558554506,
+#  'px_inj': -9.739671862890309e-06,
+#  'delta_inj': 0.0005548018233742175,
+#  'zeta_inj': -0.34736514218425374})
 
 tw_inj = line.twiss(
     betx=1, bety=1,
     x=line['x_inj'], px=line['px_inj'],
     delta=line['delta_inj'], zeta=line['zeta_inj'])
 
+class ActionTrajectory(xt.Action):
+
+    def run(self):
+        tw = line.twiss(
+            betx=1, bety=1,
+            x=line['x_inj'], px=line['px_inj'],
+            delta=line['delta_inj'], zeta=line['zeta_inj'])
+        return tw
+
+action = ActionTrajectory()
+
 tw_inj_bpms = tw_inj.rows[bpm_meas['name']]
+
+targets = []
+for nn in bpm_meas['name']:
+    targets.append(action.target('x', bpm_meas['x_meas', nn], at=nn))
+
+opt = line.match(
+    solve=False,
+    vary=[
+        xt.Vary('x_inj', step=1e-4),
+        xt.Vary('px_inj', step=1e-6),
+        xt.Vary('delta_inj', step=1e-5),
+        xt.Vary('zeta_inj', step=1e-3),
+    ],
+    targets=targets
+)
+
+opt.step(10)
+
+tw_inj_opt = line.twiss(
+    betx=1, bety=1,
+    x=line['x_inj'], px=line['px_inj'],
+    delta=line['delta_inj'], zeta=line['zeta_inj'])
 
 plt.close('all')
 plt.figure(1)
-plt.plot(tw_inj.s, tw_inj.x)
+plt.plot(tw_inj_opt.s, tw_inj_opt.x)
+plt.plot(tw_inj_bpms.s, tw_inj_opt.rows[bpm_meas['name']].x, 'o')
 plt.plot(tw_inj_bpms.s, bpm_meas['x_meas'], 'o')
+
+plt.figure(2)
+plt.plot(tw_inj_bpms.s, tw_inj_opt.rows[bpm_meas['name']].x)
+plt.plot(tw_inj_bpms.s, bpm_meas['x_meas'])
 
 plt.show()
