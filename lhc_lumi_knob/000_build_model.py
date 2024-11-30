e14d6b3571d7ce4e6cb0390a0aae6efe5e9a1489
giadarol <giovanni.iadarola@cern.ch>
2024-11-30 16:55:03 +0100

    On LHC example

--- lhc_lumi_knob/000_build_model.py
+++ lhc_lumi_knob/000_build_model.py
@@ -8,19 +8,42 @@ particle_ref = xt.Particles(energy0=6.8e12, mass0=xt.PROTON_MASS_EV)
 lhc.b1.particle_ref = particle_ref
 lhc.b2.particle_ref = particle_ref
 
+lhc.b1.cycle('ip7')
+lhc.b2.cycle('ip7_reversed') # !!!!!
+
 lhc.b2.twiss_default['reverse'] = True
 
 # Load optics
 lhc.vars.load_madx(
     "../../acc-models-lhc/strengths/ATS_Nominal/2024/ats_30cm.madx")
 
-# Wipe all orbit knobs
-for kk in lhc.vars.get_table().rows['on.*'].name:
-    lhc.vars[kk] = 0
-
 lhc['on_disp'] = 1.
-lhc['on_x1_v'] = 160.
-lhc['on_xx1_v'] = 160.
+lhc['on_xx1_v'] = 160.0
+lhc['on_x1_v'] = 160.0
+lhc['on_sep1_h'] = 0.
+lhc['on_sep2h'] = -1.0
+lhc['on_x2v'] = 200.0
+lhc['on_xx5_h'] = 160.0
+lhc['on_x5_h'] = 160.0
+lhc['on_sep5_v'] = 0.
+lhc['on_sep8h'] = -1.0
+lhc['on_x8v'] = 200.0
 
 twb1 = lhc.b1.twiss4d()
-twb1.plot('y')
+twb1.plot('x y')
+
+# opt = lhc.b1.match_knob(
+#             run=False,
+#             knob_name='shift_h_ip5.b1',
+#             ele_start='e.ds.l1.b1',
+#             ele_stop='s.ds.r1.b1',
+#             betx=1, bety=1, x=0, y=0, px=0, py=0, # <- initial conditions
+#             vary=[
+#                 xt.VaryList(['acbyvs4.l5b1', 'acbcv6.r5b1', 'acbyv4.r5b1', 'acbcv5.l5b1'],
+#                             step=1e-6),
+#             ],
+#             targets=[
+#                 xt.TargetSet(x=1e-3, y=0, px=0, py=0, at='ip5'),
+#                 xt.TargetSet(x=0, px=0, y=0, py=0, at=xt.END),
+#             ])
+
